85d27655b12bba80f26ca593da7d22753a4b25e4
JulioLlerena1 <juceller@espol.edu.ec>
2024-12-09 09:20:42 -0500

    Implementacion Clase GestorEstadoCabina

---
+++ src/main/java/GestorEstadoCabina.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorEstadoCabina {
+    
+private List<Observer> cabinasObserver = new ArrayList<>();
+
+    public void agregarCabina(Observer cabina) {
+        cabinasObserver.add(cabina);
+    }
+
+    public void borrarCabina(Observer cabina) {
+        cabinasObserver.remove(cabina);
+    }
+
+    public void notificar(String estado) {
+        for (Observer cabina : cabinasObserver) {
+            cabina.update(estado);
+        }
+    }
+
+}
